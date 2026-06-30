0f2e297276f0b98ca7c3a726bd0b4c776354a487
Amber Hou <amber.p.hou@gmail.com>
2026-06-30 11:56:05 -0700

    Firebase Git React

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'REACT_APP_')
+
+  const processEnvDefines = Object.fromEntries(
+    Object.entries(env).map(([key, value]) => [
+      `process.env.${key}`,
+      JSON.stringify(value),
+    ])
+  )
+
+  return {
+    plugins: [react()],
+    define: processEnvDefines,
+  }
 })
